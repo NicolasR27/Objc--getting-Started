044eda37cd77c8d8985253836d207e2420d81612
Nicolas Rios <nickrios33@yahoo.com>
2021-02-16 15:50:32 -0800

    Update AppDelegate.m

--- Objc -getting Started/AppDelegate.m
+++ Objc -getting Started/AppDelegate.m
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.m
-//  Objc -getting Started
-//
-//  Created by Nicolas Rios on 2/16/20.
-//  Copyright © 2020 Nicolas Rios. All rights reserved.
-//
 
 #import "AppDelegate.h"
 #import "ViewController.h"
